debf257524c1f2bb61a905558fe48eeaab05c517
Jason Barnetson <jason.barnetson@nt.gov.au>
2015-02-18 16:14:16 +1000

    Made changes to ploygon output to use seperate script

--- processing/NT_segmentation.py
+++ processing/NT_segmentation.py
@@ -28,13 +28,20 @@ def getCmdargs():
 
     p = argparse.ArgumentParser()
 
-    p.add_argument("--inimage", help="input image")
+    p.add_argument("--inimageList", help="input image")
     
+    p.add_argument("--numclusters", help="input number of clusters def = 60")
+    
+    p.add_argument("--minsize", help = "input min size def = 100")
+    
+    p.add_argument("--filetag", help="input filetag to write to end of filename, useful for testing")
+    
+    p.add_argument("--directory", help="input directory")
     
 
     cmdargs = p.parse_args()
     
-    if cmdargs.inimage is None:
+    if cmdargs.inimageList is None:
 
         p.print_help()
 
@@ -45,31 +52,38 @@ def getCmdargs():
    
     
     
-def doSegmentation(inputImage):
+def doSegmentation(images,numclust,minsize,tag,directory):
 
-	#pdb.set_trace()
-				
-	cmd  = "rscgeobia_segmentation.py -i %s" % (inputImage)
 	
-	#call raster by poly
-
-        os.system(cmd) # call it
+	filelist = []
+    
+    	with open(images, "r") as ifile:
 	
-def doVectorize(infile):
-	
-	cmd = "gdal_polygonize.py %s %s %s" % (infile,"ESRI Shapefile",infile[0:-4]+ '.shp')
-	
-	os.system(cmd)
+		for line in ifile:
+		
+		#pdb.set_trace()
+		
+			lineA = line.rstrip()
+		
+			lineB = lineA.strip('\n')
+		
+			filelist.append(lineB)
+		#pdb.set_trace()
+	for i in filelist:
+		
+		#pdb.set_trace()
+					
+		cmd  = "rscgeobia_segmentation.py -i %s --numclusters %s --minsize %s --optionfield %s" % (directory + i,numclust,minsize,tag)
+		
+		os.system(cmd) # call it
+						
 
 def mainRoutine():
 	
 	cmdargs = getCmdargs() # instantiate the get command line function
 	
-	inputImage = cmdargs.inimage
+	doSegmentation(cmdargs.inimageList,cmdargs.numclusters,cmdargs.minsize,cmdargs.filetag,cmdargs.directory)
 	
-	doSegmentation(inputImage)
-	
-	doVectorize(inputImage)
 	
 
 
